27fb10187d28b49bdf78387b20f27c288bb3e2c8
jakepens71 <jake_ps@comcast.net>
2015-08-25 23:44:58 -0400

    added npm yeoman

--- rvmInstall.sh
+++ rvmInstall.sh
@@ -85,3 +85,8 @@ sudo add-apt-repository ppa:webupd8team/sublime-text-2
 sudo apt-get update
 
 sudo apt-get install sublime-text
+
+echo "Installing gruny bower yeoman"
+
+npm install -g yo bower grunt-cli gulp
+
